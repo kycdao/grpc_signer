7723334250066e799d94b46f08a74069030bee69
Juhasz Sandor <blackghost1987@gmail.com>
2022-05-20 13:04:21 +0200

    separate codegen into sub-crate

--- codegen/src/main.rs
+++ codegen/src/main.rs
@@ -1,10 +1,12 @@
 use std::path::PathBuf;
 
 fn main() {
-    let out_dir = PathBuf::from("src");
+    print!("Generating Protobuf code...");
+    let out_dir = PathBuf::from("../src");
     tonic_build::configure()
         .out_dir(out_dir)
         //.file_descriptor_set_path(out_dir.join("signer_descriptor.bin"))
         .compile(&["proto/signer.proto"], &["proto"])
         .unwrap();
+    println!(" Done!");
 }
